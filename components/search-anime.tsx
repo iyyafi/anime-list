43f60481a5cc6c6e2fff536014d59267533695f7
iyyafi <iyyafi@gmail.com>
2023-07-13 11:14:31 +0700

    add change search param

--- components/search-anime.tsx
+++ components/search-anime.tsx
@@ -1,3 +1,25 @@
+'use client'
+import { useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
 export default function SearchAnime() {
-    return <input type="text" className="p-1 border-gray-300 border rounded" />
+    const router = useRouter()
+    const pathname = usePathname()
+    const [searchQuery, setSearchQuery] = useState('')
+
+    function updateSearchQuery(e: any) {
+        if (e.key === 'Enter') {
+            const query = searchQuery ? `?q=${searchQuery}` : ''
+            router.push(`${pathname}${query}`)
+        }
+    }
+
+    return (
+        <input
+            type="text"
+            className="p-1 border-gray-300 border rounded"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={(e) => updateSearchQuery(e)}
+        />
+    )
 }
